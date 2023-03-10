b5efefa2d3c5db563fb8c3913fed5de77d7b38be
Taner <taneozer16@gmail.com>
2023-03-10 19:12:10 +0300

    twitterController and  facebookController

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const authController = require('../controllers/authController');
 const googleLoginController = require('../controllers/google-login-controller');
 const profileController = require('../controllers/profileController');
+const twitterController = require('../controllers/twitterController');
+const facebookController = require('../controllers/facebookController');
 const jwtMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
@@ -16,6 +18,11 @@ router.post('/google-login',googleLoginController.googleLogin);
 // router.get('/googleCallback',googleLoginController.googleCallback);
 
 router.post('/linkedin-login', authController.linkedinLogin);
+
+router.post('/twitter', twitterController.twitterLogin);
+
+router.post('/facebook', facebookController.facebookLogin);
+
 
 router.patch('/profile/:id', profileController.updateProfile);
 
